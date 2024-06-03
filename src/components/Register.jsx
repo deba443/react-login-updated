6fda59cf5b996ff15e100cb6c59ef4f916c94b4e
deba443 <debaprasad443@gmail.com>
2024-06-03 20:47:59 +0530

    Changed Register form and register style

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -14,9 +14,20 @@ const Register = ({ show, setShow ,onClose}) => {
       <h2 className={style.upperFont}>Welcome on board!</h2>
       <h3 className={style.upperFont}>We just need little bit of data to get started</h3>
       <form >
-        <input type="email"/>
-        <input type="password"/>
-        <input type="password"/>
+        <div>
+          <label htmlFor="email">email</label>
+          <input type="email" name="email"/>
+        </div>
+        <div className={style.passwordContainer}>
+          <div>
+        <label htmlFor="current-password">Password</label>
+        <input type="password" name="current-password"/>
+        </div>
+        <div>
+        <label htmlFor="confirm-password">confirm-password</label>
+        <input type="password" name="confirm-password"/>
+        </div>
+        </div>
         <input type="text"/>
         <input type="text"/>
         <select name="role" id="role">
